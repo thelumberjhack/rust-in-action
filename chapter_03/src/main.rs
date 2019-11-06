5487cd3c9a918b1cc28c7dc0b12d848578a63b06
Yannick Formaggio <yannick.formaggio@gmail.com>
2019-11-06 14:38:01 -0800

    Done with chapter 3

--- chapter_03/src/main.rs
+++ chapter_03/src/main.rs
@@ -1,3 +1,93 @@
+//! Simulating files one step at a time
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+pub enum FileState {
+    Open,
+    Closed,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+/// Represents a file, which probably lives on a filesystem.
+#[derive(Debug)]
+pub struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File {
+    /// New files are assumed to be empty, but a name is required.
+    /// 
+    /// # Examples
+    /// 
+    /// ```no_run
+    /// let f  = File::new("f1.txt");
+    /// ```
+    pub fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+
+    /// Reads a file and returns the number of bytes read.
+    pub fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File is not opened"));
+        }
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
+    Ok(f)
+}
+
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
+    Ok(f)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f3 = File::new("f3.txt");
+    
+    let mut buffer: Vec<u8> = vec![];
+
+    if f3.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+    
+    f3 = open(f3).unwrap();
+    println!("{}", f3);
+    let f3_length = f3.read(&mut buffer).unwrap();
+    f3 = close(f3).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text);
+    println!("{}", f3);
 }
