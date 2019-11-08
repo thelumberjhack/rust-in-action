e340459771f95487e2644a4e0d8ddf518ef2e8fb
Yannick Formaggio <yannick.formaggio@gmail.com>
2019-11-08 11:59:42 -0800

    decoding a float

---
+++ chapter_05/src/main.rs
@@ -0,0 +1,45 @@
+const BIAS: i32 = 127;
+const RADIX: f32 = 2.0;
+
+fn deconstruct_f32(n: f32) -> (u32, u32, u32) {
+    let n_: u32 = unsafe { std::mem::transmute(n) };
+
+    let sign = (n_ >> 31) & 1;
+    let exponent = (n_ >> 23) & 0xff;
+    let fraction = 0b00000000_01111111_11111111_11111111 & n_;
+
+    (sign, exponent, fraction)
+}
+
+fn decode_f32_parts(sign: u32, exponent: u32, fraction: u32) -> (f32, f32, f32) {
+    let signed_1 = (-1.0_f32).powf(sign as f32);
+
+    let exponent = (exponent as i32) - BIAS;
+    let exponent = RADIX.powf(exponent as f32);
+
+    let mut mantissa: f32 = 1.0;
+
+    for i in 0..23_u32 {
+        let one_at_bit_1 = 1 << i;
+        if (one_at_bit_1 & fraction) != 0 {
+            mantissa += 2_f32.powf((i as f32) - 23.0);
+        }
+    }
+    (signed_1, exponent, mantissa)
+}
+
+fn f32_from_parts(sign: f32, exponent: f32, mantissa: f32) -> f32 {
+    sign * exponent * mantissa
+}
+
+fn main() {
+    let n: f32 = 42.42;
+
+    let (signbit, exponent, fraction) = deconstruct_f32(n);
+    let (sign, exponent, mantissa) = decode_f32_parts(signbit, exponent, fraction);
+    let reconst_n = f32_from_parts(sign, exponent, mantissa);
+
+    println!("{} -> [sign: {}, exponent: {}, mantissa: {:?}] -> {}",
+                n, signbit, exponent, mantissa, reconst_n
+    );
+}
