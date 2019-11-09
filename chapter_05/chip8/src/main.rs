21694e3702f9fce6ddd48ac80b8d1fd97370b170
Yannick Formaggio <yannick.formaggio@gmail.com>
2019-11-08 16:37:08 -0800

    More functions

--- chapter_05/chip8/src/main.rs
+++ chapter_05/chip8/src/main.rs
@@ -1,13 +1,12 @@
-// Defined by CHIP-8 documentation
-const ARITHMETIC_AND_LOGIC: u8 = 0x8;
-const HALT: u8 = 0x0;
-const ADD_XY: u8 = 0x4;
+/// CHIP-8 Emulator
 
 struct CPU {
     // current_operation: u16,
     registers: [u8; 16],
     position_in_memory: usize,
     memory: [u8; 4096],         // 4kb
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -16,23 +15,50 @@ impl CPU {
             // let encoded_op = self.current_operation;
             let op_byte1 = self.memory[self.position_in_memory] as u16;
             let op_byte2 = self.memory[self.position_in_memory + 1] as u16;
-            let raw_op = op_byte1 << 8 | op_byte2;
+            let opcode = op_byte1 << 8 | op_byte2;
 
-            let op_major = ((raw_op & 0xf000) >> 12) as u8;
-            let x = ((raw_op & 0x0f00) >> 8) as u8;
-            let y = ((raw_op & 0x00f0) >> 4) as u8;
-            let op_minor = (raw_op & 0xf) as u8;
+            let x = ((opcode & 0x0f00) >> 8) as u8;
+            let y = ((opcode & 0x00f0) >> 4) as u8;
+            let op_minor = (opcode & 0xf) as u8;
+            let addr = opcode & 0x0fff;
 
             self.position_in_memory += 2;
 
-            match (op_major, op_minor) {
-                (HALT, HALT) => { return; },
-                (ARITHMETIC_AND_LOGIC, ADD_XY) => {
-                    self.add_xy(x, y);
-                },
-                _ => unimplemented!("opcode {:04x}", raw_op),
+            match opcode {
+                0x0000 => { return; },
+                0x00ee => { self.ret(); },
+                0x2000..=0x2fff => { self.call(addr); },
+                0x8000..=0x8fff => {
+                    match op_minor {
+                        4 => { self.add_xy(x, y); },
+                        _ => { unimplemented!("opcode: {:04x}", opcode); },
+                    }
+                }
+                _ => unimplemented!("opcode {:04x}", opcode),
             }
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack Overfl0w!!!");
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow!");
+        }
+
+        self.stack_pointer -= 1;
+        self.position_in_memory = self.stack[self.stack_pointer] as usize;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
@@ -48,22 +74,25 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     // Initialize registers and memory
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
-    cpu.memory[0] = 0x80; cpu.memory[1] = 0x14;
-    cpu.memory[2] = 0x80; cpu.memory[3] = 0x24;
-    cpu.memory[4] = 0x80; cpu.memory[5] = 0x34;
+    cpu.memory[0x000] = 0x21; cpu.memory[0x001] = 0x00;
+    cpu.memory[0x002] = 0x21; cpu.memory[0x003] = 0x00;
+
+    cpu.memory[0x100] = 0x80; cpu.memory[0x101] = 0x14;
+    cpu.memory[0x102] = 0x80; cpu.memory[0x103] = 0x14;
+    cpu.memory[0x104] = 0x00; cpu.memory[0x105] = 0xee;
 
     // Run
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
